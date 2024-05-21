1c7b58c2b7da11f2787b2af67006ee53760105a4
rahmonjones <rahmon.jay1@gmail.com>
2024-05-21 09:56:07 +0100

    new update

--- myfirst.sh
+++ myfirst.sh
@@ -1,3 +1,10 @@
 echo "hello friend"
+ray: i'm fine beautiful lol
 echo "How are you ?"
+ray: i'm very well fine, thanks for asking. Trust you've been well too?
 echo "what is your age?"
+ray: lol, well im as old as Mongopark.
+
+what are you doing this weekend?
+summer is back! a good time to be at the beach
+if your weekend is not too busy lets have some good time
